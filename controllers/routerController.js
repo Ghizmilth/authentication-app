34bc02adfa78e680182fa50aafd7aeffb7dde16a
Ghizmilth <hidanerferrer@gmail.com>
2018-02-20 13:27:33 -0800

    added middleware and my app.js files

--- controllers/routerController.js
+++ controllers/routerController.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
+var User = require("../models/users");
 
+// GET route for reading data
 router.get("/", function(req, res, next) {
   return res.sendFile(path.join(__dirname + "/views/index.html"));
 });
 
+//POST route for updating data
 router.post("/", function(req, res, next) {
   if (req.body.password !== req.body.passConfirmation) {
     var err = new Error("Passwords do not match.");
@@ -51,6 +54,43 @@ router.post("/", function(req, res, next) {
     var err = new Error("All fields are requiered.");
     err.status = 400;
     return next(err);
+  }
+});
+
+// GET route after registering
+router.get("/profile", function(req, res, next) {
+  User.findById(req.session.userId).exec(function(error, user) {
+    if (error) {
+      return next(error);
+    } else {
+      if (user === null) {
+        var err = new Error("Not authorized! Go back!");
+        err.status = 400;
+        return next(err);
+      } else {
+        return res.send(
+          "<h1>Name: </h1>" +
+            user.username +
+            "<h2>Mail: </h2>" +
+            user.email +
+            '<br><a type="button" href="/logout">Logout</a>'
+        );
+      }
+    }
+  });
+});
+
+// GET for logout logout
+router.get("/logout", function(req, res, next) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(function(err) {
+      if (err) {
+        return next(err);
+      } else {
+        return res.redirect("/");
+      }
+    });
   }
 });
 
--- models/users.js
+++ models/users.js
@@ -17,10 +17,42 @@ var UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
-  passwordConf: {
+  passConfirmation: {
     type: String,
     required: true
   }
+});
+
+//authenticate input against database
+UserSchema.statics.authenticate = function (email, password, callback) {
+  User.findOne({email: email}.exec(function (err, user) {
+    if (err) {
+      return callback(err)
+    } else if (user) {
+      var err = new Error('User not found.');
+      err.status = 401;
+      return callback(err);
+    }
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (result === true) {
+        return callback(null, user);
+      } else {
+        return calllback();
+      }
+    })
+  });
+}
+
+//hashing a password before saving
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash) {
+    if (err) {
+      return next (err);
+    }
+    user.password = hash;
+    next();
+  })
 });
 
 var User = mongoose.model("User", UserSchema);
