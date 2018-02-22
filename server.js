d8f4a07b4596757197f97921d5c8648d17fd46ae
Ghizmilth <hidanerferrer@gmail.com>
2018-02-22 15:52:47 -0800

    changed mongoDB connection on server file

--- server.js
+++ server.js
@@ -5,26 +5,19 @@ var mongoose = require("mongoose");
 var mongodb = reuire ('mongodb');
 var session = require("express-session");
 var MongoStore = require("connect-mongo")(session);
-// var uri =
-//   "mongodb://heroku_0c85xskw:qn26c4jp9ntk425dojtadb7opp@ds245518.mlab.com:45518/heroku_0c85xskw";
-// var uri =
 
-// //connect to MongoDB
-// mongoose.connect(uri);
 
 // serve static files from template
 app.use(express.static(__dirname + "/views"));
 // parse incoming requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 //handle mongo error
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
   // we're connected!
-});
-
-
+// });
 var db;
 
 // Connect to the database before starting the application server.
@@ -66,10 +59,4 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.send(err.message);
-});
-
-// listen on port 3000
-app.listen(process.env.PORT || 8080, function() {
-  var port = server.address().port;
-  console.log("Express app listening on port 8080", port);
 });
