806a1f46329f0bfba1a6ad0599d3ccabf615bf50
Ghizmilth <hidanerferrer@gmail.com>
2018-02-27 21:59:54 -0800

    changed some dependencies/server.js updated

--- server.js
+++ server.js
@@ -2,66 +2,57 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var mongodb = require("mongodb");
 var session = require("express-session");
 var MongoStore = require("connect-mongo")(session);
 
-// serve static files from template
-app.use(express.static(__dirname + "/views"));
-// parse incoming requests
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+//connect to MongoDB
+mongoose.connect("mongodb://localhost/authentication-app");
+var db = mongoose.connection;
 
 //handle mongo error
-// db.on("error", console.error.bind(console, "connection error:"));
-// db.once("open", function() {
-// we're connected!
-// });
-var db;
-
-// Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-  }
-  // Save database object from the callback for reuse.
-  db = database;
-  console.log("Database connection ready");
-
-  //use sessions for tracking logins
-  app.use(
-    session({
-      secret: "work hard",
-      resave: true,
-      saveUninitialized: false,
-      store: new MongoStore({
-        mongooseConnection: db
-      })
-    })
-  );
-
-  var server = app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-
-  var routes = require("./controllers/routerController");
-
-  app.use("/", routes);
-
-  // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error("File Not Found");
-    err.status = 404;
-    next(err);
-  });
-
-  // error handler
-  // define as the last app.use callback
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.send(err.message);
-  });
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
+  // "we're connected!"
 });
+
+//use sessions for tracking logins
+app.use(
+  session({
+    secret: "pizza rock",
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: db
+    })
+  })
+);
+
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// serve static files from template
+app.use(express.static(__dirname + "/views"));
+
 // include routes
+var routes = require("./controllers/routerController");
+app.use("/", routes);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error("File Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+// define as the last app.use callback
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log("Express app listening on port 3000");
+});
